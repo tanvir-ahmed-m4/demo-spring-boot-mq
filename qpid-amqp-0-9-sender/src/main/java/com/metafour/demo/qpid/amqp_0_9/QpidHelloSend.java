a8ac2bcd364cbf00b0975b37631744fdc66b2707
tanvir-ahmed-m4 <tanvir.ahmed@metafour.com>
2015-01-21 17:17:47 +0600

    Working with RabbitMQ (AMQP v0.9) with virtual host, exchange and queue

--- qpid-amqp-0-9-sender/src/main/java/com/metafour/demo/qpid/amqp_0_9/QpidHelloSend.java
+++ qpid-amqp-0-9-sender/src/main/java/com/metafour/demo/qpid/amqp_0_9/QpidHelloSend.java
@@ -3,41 +3,46 @@ package com.metafour.demo.qpid.amqp_0_9;
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+
+import org.apache.log4j.Logger;
+
 import java.util.Properties;
 
 public class QpidHelloSend {
+  
+  private static final Logger logger = Logger.getLogger(QpidHelloSend.class);
+  
+  public QpidHelloSend() {}
 
-	public QpidHelloSend() {
-	}
+  public static void main(String[] args) {
+    QpidHelloSend producer = new QpidHelloSend();
+    producer.runTest();
+  }
 
-	public static void main(String[] args) {
-		QpidHelloSend producer = new QpidHelloSend();
-		producer.runTest();
-	}
+  private void runTest() {
+    try {
+      Properties properties = new Properties();
+      properties.load(this.getClass().getResourceAsStream("/hello.properties"));
+      Context context = new InitialContext(properties);
 
-	private void runTest() {
-		try {
-			Properties properties = new Properties();
-			properties.load(this.getClass().getResourceAsStream("/hello.properties"));
-			Context context = new InitialContext(properties);
+      ConnectionFactory connectionFactory =
+          (ConnectionFactory) context.lookup("qpidConnectionfactory");
+      Connection connection = connectionFactory.createConnection();
+      connection.start();
 
-			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("qpidConnectionfactory");
-			Connection connection = connectionFactory.createConnection();
-			connection.start();
+      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Destination destination = (Destination) context.lookup("topicExchange");
 
-			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			Destination destination = (Destination) context.lookup("topicExchange");
+      MessageProducer messageProducer = session.createProducer(destination);
 
-			MessageProducer messageProducer = session.createProducer(destination);
+      TextMessage message = session.createTextMessage("Hello AMQP 0.9 world!");
+      messageProducer.send(message);
+      logger.info("Message sent!");
 
-			TextMessage message = session.createTextMessage("Hello AMQP 0.9 world!");
-			messageProducer.send(message);
-			System.out.println("Message sent!");
-
-			connection.close();
-			context.close();
-		} catch (Exception exp) {
-			exp.printStackTrace();
-		}
-	}
+      connection.close();
+      context.close();
+    } catch (Exception e) {
+      logger.fatal(e.getMessage(), e);
+    }
+  }
 }
