b5324d15a46bfdea1fac2669d34a2aa9b3a2fa7b
tanvir-ahmed-m4 <tanvir.ahmed@metafour.com>
2015-01-21 17:33:52 +0600

    Broken with RabbitMQ (AMQP v0.9) with virtual host, exchange and queue

--- qpid-amqp-0-9-receiver/src/main/java/com/metafour/demo/qpid/amqp_0_9/QpidHelloRecv.java
+++ qpid-amqp-0-9-receiver/src/main/java/com/metafour/demo/qpid/amqp_0_9/QpidHelloRecv.java
@@ -1,42 +1,52 @@
 package com.metafour.demo.qpid.amqp_0_9;
 
-import javax.jms.*;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import java.util.Properties;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import org.apache.log4j.Logger;
+
 public class QpidHelloRecv {
+  
+  private static final Logger logger = Logger.getLogger(QpidHelloRecv.class);
 
-	public QpidHelloRecv() {
-	}
+  public QpidHelloRecv() {}
 
-	public static void main(String[] args) {
-		QpidHelloRecv producer = new QpidHelloRecv();
-		producer.runTest();
-	}
+  public static void main(String[] args) {
+    QpidHelloRecv producer = new QpidHelloRecv();
+    producer.runTest();
+  }
 
-	private void runTest() {
-		try {
-			Properties properties = new Properties();
-			properties.load(this.getClass().getResourceAsStream("/hello.properties"));
-			Context context = new InitialContext(properties);
+  private void runTest() {
+    try {
+      Properties properties = new Properties();
+      properties.load(this.getClass().getResourceAsStream("/hello.properties"));
+      Context context = new InitialContext(properties);
 
-			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("qpidConnectionfactory");
-			Connection connection = connectionFactory.createConnection();
-			connection.start();
+      ConnectionFactory connectionFactory =
+          (ConnectionFactory) context.lookup("qpidConnectionfactory");
+      Connection connection = connectionFactory.createConnection();
+      connection.start();
 
-			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			Destination destination = (Destination) context.lookup("topicExchange");
+      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Destination destination = (Destination) context.lookup("topicExchange");
 
-			MessageConsumer messageConsumer = session.createConsumer(destination);
+      MessageConsumer messageConsumer = session.createConsumer(destination);
 
-			TextMessage message = (TextMessage) messageConsumer.receive();
-			System.out.println("Message received: " + message.getText());
+      TextMessage message = (TextMessage) messageConsumer.receive();
+      logger.info("Message received: " + message.getText());
 
-			connection.close();
-			context.close();
-		} catch (Exception exp) {
-			exp.printStackTrace();
-		}
-	}
+      connection.close();
+      context.close();
+    } catch (Exception e) {
+      logger.fatal(e.getMessage(), e);
+    }
+  }
 }
