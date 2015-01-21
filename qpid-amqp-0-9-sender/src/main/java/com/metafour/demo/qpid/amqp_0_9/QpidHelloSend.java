afa2e40a51370b50d671cd54a5fbb16725027038
tanvir-ahmed-m4 <tanvir.ahmed@metafour.com>
2015-01-21 14:31:28 +0600

    Qpid JMS AMQP 0.x Sender Sample with ActiveMQ

---
+++ qpid-amqp-0-9-sender/src/main/java/com/metafour/demo/qpid/amqp_0_9/QpidHelloSend.java
@@ -0,0 +1,43 @@
+package com.metafour.demo.qpid.amqp_0_9;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Properties;
+
+public class QpidHelloSend {
+
+	public QpidHelloSend() {
+	}
+
+	public static void main(String[] args) {
+		QpidHelloSend producer = new QpidHelloSend();
+		producer.runTest();
+	}
+
+	private void runTest() {
+		try {
+			Properties properties = new Properties();
+			properties.load(this.getClass().getResourceAsStream("/hello.properties"));
+			Context context = new InitialContext(properties);
+
+			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("qpidConnectionfactory");
+			Connection connection = connectionFactory.createConnection();
+			connection.start();
+
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = (Destination) context.lookup("topicExchange");
+
+			MessageProducer messageProducer = session.createProducer(destination);
+
+			TextMessage message = session.createTextMessage("Hello AMQP 0.9 world!");
+			messageProducer.send(message);
+			System.out.println("Message sent!");
+
+			connection.close();
+			context.close();
+		} catch (Exception exp) {
+			exp.printStackTrace();
+		}
+	}
+}
