050621455b791702c5ea2c1fcae6a45f8a693fc0
tanvir-ahmed-m4 <tanvir.ahmed@metafour.com>
2015-01-22 21:09:01 +0600

    Qpid JMS 1.1 over AMQP 1.0 working with ActiveMQ and Qpid Java broker

--- qpid-amqp-1-0-receiver/src/main/java/com/metafour/demo/qpid/amqp_1_0/QpidHelloRecv.java
+++ qpid-amqp-1-0-receiver/src/main/java/com/metafour/demo/qpid/amqp_1_0/QpidHelloRecv.java
@@ -1,42 +1,84 @@
 package com.metafour.demo.qpid.amqp_1_0;
 
-import javax.jms.*;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import java.util.Properties;
+
+import org.apache.log4j.Logger;
 
 public class QpidHelloRecv {
 
-	public QpidHelloRecv() {
-	}
+  private static final Logger logger = Logger.getLogger(QpidHelloRecv.class);
 
-	public static void main(String[] args) {
-		QpidHelloRecv producer = new QpidHelloRecv();
-		producer.runTest();
-	}
+  public QpidHelloRecv() {}
 
-	private void runTest() {
-		try {
-			Properties properties = new Properties();
-			properties.load(this.getClass().getResourceAsStream("/hello.properties"));
-			Context context = new InitialContext(properties);
+  public static void main(String[] args) {
+    QpidHelloRecv producer = new QpidHelloRecv();
+    producer.runTest();
+  }
 
-			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("qpidConnectionfactory");
-			Connection connection = connectionFactory.createConnection();
-			connection.start();
+  private void runTest() {
+    try (InputStream propertiesStream = getClass().getResourceAsStream("/hello.properties")) {
+      Properties properties = new Properties();
+      // Read the hello.properties JNDI properties file and use contents to create the
+      // InitialContext.
+      properties.load(propertiesStream);
+      Context context = new InitialContext(properties);
+      // Alternatively, JNDI information can be supplied by setting the
+      // "java.naming.factory.initial"
+      // system property to value
+      // "org.apache.qpid.amqp_1_0.jms.jndi.PropertiesFileInitialContextFactory"
+      // and setting the "java.naming.provider.url" system property as a URL to a properties file.
 
-			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			Destination destination = (Destination) context.lookup("topicExchange");
+      ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("localhost");
+      Connection connection = connectionFactory.createConnection();
 
-			MessageConsumer messageConsumer = session.createConsumer(destination);
+      Queue queue = (Queue) context.lookup("queue");
 
-			TextMessage message = (TextMessage) messageConsumer.receive();
-			System.out.println("Message received: " + message.getText());
+      Session consumerSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer messageConsumer = consumerSession.createConsumer(queue);
 
-			connection.close();
-			context.close();
-		} catch (Exception exp) {
-			exp.printStackTrace();
-		}
-	}
+      final CountDownLatch latch = new CountDownLatch(1);
+      messageConsumer.setMessageListener(new MessageListener() {
+        public void onMessage(final Message message) {
+          try {
+            if (message instanceof TextMessage) {
+              logger.info("Received message: " + ((TextMessage) message).getText());
+            } else {
+              logger.info("Received enexpected message type: " + message.getClass().getName());
+            }
+
+            latch.countDown();
+          } catch (JMSException e) {
+            logger.info("Caught exception in onMessage(): " + e.getMessage());
+          }
+        }
+      });
+
+      connection.start();
+
+      int delay = 5;
+      if (!latch.await(delay, TimeUnit.SECONDS)) {
+        logger.info("Waited " + delay + " sec but no message recieved.");
+      }
+
+      connection.close();
+      context.close();
+    } catch (Exception exp) {
+      logger.fatal("Caught exception: " + exp.getMessage(), exp);
+    }
+  }
 }
--- qpid-amqp-1-0-sender/src/main/java/com/metafour/demo/qpid/amqp_1_0/QpidHelloSend.java
+++ qpid-amqp-1-0-sender/src/main/java/com/metafour/demo/qpid/amqp_1_0/QpidHelloSend.java
@@ -1,43 +1,67 @@
 package com.metafour.demo.qpid.amqp_1_0;
 
-import javax.jms.*;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import java.util.Properties;
+
+import org.apache.log4j.Logger;
 
 public class QpidHelloSend {
 
-	public QpidHelloSend() {
-	}
+  private static final Logger logger = Logger.getLogger(QpidHelloSend.class);
 
-	public static void main(String[] args) {
-		QpidHelloSend producer = new QpidHelloSend();
-		producer.runTest();
-	}
+  public QpidHelloSend() {}
 
-	private void runTest() {
-		try {
-			Properties properties = new Properties();
-			properties.load(this.getClass().getResourceAsStream("/hello.properties"));
-			Context context = new InitialContext(properties);
+  public static void main(String[] args) {
+    QpidHelloSend producer = new QpidHelloSend();
+    producer.runTest();
+  }
 
-			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("qpidConnectionfactory");
-			Connection connection = connectionFactory.createConnection();
-			connection.start();
+  private void runTest() {
+    try (InputStream propertiesStream = getClass().getResourceAsStream("/hello.properties")) {
+      Properties properties = new Properties();
+      // Read the hello.properties JNDI properties file and use contents to create the
+      // InitialContext.
+      properties.load(propertiesStream);
+      Context context = new InitialContext(properties);
+      // Alternatively, JNDI information can be supplied by setting the
+      // "java.naming.factory.initial"
+      // system property to value
+      // "org.apache.qpid.amqp_1_0.jms.jndi.PropertiesFileInitialContextFactory"
+      // and setting the "java.naming.provider.url" system property as a URL to a properties file.
 
-			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			Destination destination = (Destination) context.lookup("topicExchange");
+      ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("localhost");
+      Connection connection = connectionFactory.createConnection();
 
-			MessageProducer messageProducer = session.createProducer(destination);
+      Session producersession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Queue queue = (Queue) context.lookup("queue");
+//      Destination destination = (Destination) context.lookup("topicExchange");
 
-			TextMessage message = session.createTextMessage("Hello world!");
-			messageProducer.send(message);
-			System.out.println("Message sent!");
 
-			connection.close();
-			context.close();
-		} catch (Exception exp) {
-			exp.printStackTrace();
-		}
-	}
+      connection.start();
+
+      MessageProducer queueProducer = producersession.createProducer(queue);
+//      MessageProducer destinationProducer = producersession.createProducer(destination);
+
+      TextMessage message = producersession.createTextMessage("Hello AMQP 1.0 world at " + System.currentTimeMillis() + "!");
+
+      queueProducer.send(message);
+//      destinationProducer.send(message);
+      logger.info("Message sent!");
+
+      connection.close();
+      context.close();
+    } catch (Exception exp) {
+      logger.fatal("Caught exception: " + exp.getMessage(), exp);
+    }
+  }
 }
